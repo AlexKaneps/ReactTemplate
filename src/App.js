fe3f873334bd253a8adf5c42bb398a8267993ad7
Alex Kaneps <alexkaneps@Alexs-MacBook-Air.local>
2022-10-30 13:20:53 -0500

    create basic react template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading__one">React application!!!</h1>
+    </div>
+  );
+}
+
+export default App;
